a4fc331e6fb89223934bc0698e8cb8d75b782d1c
Luis Cheung <lcheung90@gmail.com>
2013-03-28 19:40:36 -0700

    Initial commit
    Barebone files and file strusture created by rails

---
+++ UPE_site/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UPESite::Application.config.secret_token = 'd4fe2021087668b333dc4a3cc0020596dc47203569486d44800273f339c86d48e5bab192ea0a66e86dab22d3dabb0048093d85489de7769a55e055de7e7a2b32'
