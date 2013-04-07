aa25877dff97966cf071aa381b39680b71339f33
Luis Cheung <lcheung90@gmail.com>
2013-04-07 11:54:36 -0700

    Modify user creation form

--- UPE_site/app/models/user.rb
+++ UPE_site/app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ActiveRecord::Base
-	has_secure_password
+	#has_secure_password
 	validates :email, :presence => true,
 					  :uniqueness => true,
 					  :format => {:with => /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})/}
 	validates :name, :presence => true
 	validates :password_digest, :presence => true,
 								:length => {:minimum => 8}
-	validates :username, :presence => true
+	validates :username, :presence => true,
 						 :uniqueness => true
-	has_many :questions,
-	has_many :answers,
+	has_many :questions
+	has_many :answers
 	attr_accessible :email, :name, :password_digest, :password_confirmation, :username
 end
