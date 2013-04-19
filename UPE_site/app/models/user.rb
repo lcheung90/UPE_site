714ec08867bb3bb01d09c29298da1a255d7a6d9f
Luis Cheung <lcheung90@gmail.com>
2013-04-19 10:46:24 -0700

    Add password encryption

--- UPE_site/app/models/user.rb
+++ UPE_site/app/models/user.rb
@@ -4,12 +4,12 @@ class User < ActiveRecord::Base
 					  :uniqueness => true,
 					  :format => {:with => /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})/}
 	validates :name, :presence => true
-	validates :password_digest, :presence => true,
+	validates :password, :presence => true,
 								:length => {:minimum => 8}
 	validates :username, :presence => true,
 						 :uniqueness => true
 	has_many :questions
 	has_many :answers
 	has_many :comments
-	attr_accessible :email, :name, :password_digest, :username
+	attr_accessible :email, :name, :password, :username
 end
